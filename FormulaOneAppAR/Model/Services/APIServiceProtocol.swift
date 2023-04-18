a03c68b88ac123a9f8f354b6a9a6a61ddc499498
siwarnafti <76211002+siwarnafti@users.noreply.github.com>
2023-04-18 02:42:47 +0100

    change password and some ui arrangement

---
+++ FormulaOneAppAR/Model/Services/APIServiceProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  APIServiceProtocol.swift
+//  FormulaOneAppAR
+//
+//  Created by Siwar Nafti on 14/4/2023.
+//
+
+import Foundation
---
+++ FormulaOneAppAR/View/Team/TeamView.swift
@@ -0,0 +1,20 @@
+//
+//  TeamView.swift
+//  FormulaOneAppAR
+//
+//  Created by Siwar Nafti on 14/4/2023.
+//
+
+import SwiftUI
+
+struct TeamView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TeamView_Previews: PreviewProvider {
+    static var previews: some View {
+        TeamView()
+    }
+}
